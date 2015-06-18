c004ff71971ab38414f586b86a536712c4f773ac
Kaio Cristian Costa <kaio.zeroglosa>
2015-06-18 11:21:51 -0300

    Adding home in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  get 'home' => 'static#home'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
