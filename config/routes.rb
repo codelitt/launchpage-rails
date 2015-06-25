683c3a2eb49d976b514a6d514576504cf6745274
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-06-24 23:54:55 -0300

    adding csv route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users
   get 'static/home'
   get 'success' => 'static#success'
+  get 'csv' => 'users#export_csv', :as => :csv
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
